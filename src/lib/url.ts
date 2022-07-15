447ca4ec44c173c03bc330349c2d8b4e8427ad43
novacbn <31122674+novacbn@users.noreply.github.com>
2022-07-15 17:15:04 -0400

    - Added `IURLStore.navigate` / `IURLStorage.update` to support history data caching

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,7 +1,11 @@
 import type {Readable} from "svelte/store";
 import {readable} from "svelte/store";
 
-export type IURLStore = Readable<URL>;
+export interface IURLStore extends Readable<URL> {
+    navigate<T = any>(href: string, state?: T): void;
+
+    update<T = any>(state?: T): void;
+}
 
 function get_hash_url(): URL {
     return new URL(window.location.hash.slice(1) || "/", window.location.origin);
@@ -12,22 +16,51 @@ function get_location_url(): URL {
 }
 
 export function hash(): IURLStore {
-    return readable(get_hash_url(), (set) => {
-        function on_popstate(): void {
+    const {subscribe} = readable<URL>(get_hash_url(), (set) => {
+        function on_pop_state(): void {
             const url = get_hash_url();
 
             set(url);
         }
 
-        on_popstate();
+        on_pop_state();
 
-        window.addEventListener("popstate", on_popstate);
-        return () => window.removeEventListener("popstate", on_popstate);
+        window.addEventListener("popstate", on_pop_state);
+        return () => window.removeEventListener("popstate", on_pop_state);
     });
+
+    return {
+        navigate(href, state = undefined) {
+            const {pathname, origin} = window.location;
+            const {hash, pathname: href_pathname, search} = new URL(href, origin);
+
+            history.pushState(
+                state,
+                "",
+                `${origin}/${pathname.slice(1)}#${href_pathname}${search}${hash}`
+            );
+
+            window.dispatchEvent(new PopStateEvent("popstate"));
+        },
+
+        subscribe,
+
+        update(state = undefined) {
+            const {hash, pathname, origin} = window.location;
+
+            history.replaceState(
+                state,
+                "",
+                `${origin}/${pathname.slice(1)}#${hash.slice(1) || "/"}`
+            );
+
+            window.dispatchEvent(new PopStateEvent("popstate"));
+        },
+    };
 }
 
 export function location(): IURLStore {
-    return readable(get_location_url(), (set) => {
+    const {subscribe} = readable<URL>(get_location_url(), (set) => {
         function on_popstate(): void {
             const url = get_location_url();
 
@@ -39,4 +72,21 @@ export function location(): IURLStore {
         window.addEventListener("popstate", on_popstate);
         return () => window.removeEventListener("popstate", on_popstate);
     });
+
+    return {
+        navigate(href, state = undefined) {
+            const {origin} = window.location;
+            const {hash, pathname, search} = new URL(href, origin);
+
+            history.pushState(state, "", `${origin}/${pathname.slice(1)}${search}${hash}`);
+        },
+
+        subscribe,
+
+        update(state = undefined) {
+            const {hash, pathname, search, origin} = window.location;
+
+            history.replaceState(state, "", `${origin}/${pathname.slice(1)}${search}${hash}`);
+        },
+    };
 }
